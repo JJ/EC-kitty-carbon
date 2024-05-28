299582f162af9ac7914425ce4093c063a150b926
JJ Merelo <jjmerelo@gmail.com>
2024-05-28 10:31:55 +0200

    Ready for EA2P

--- eckity_carbon/__init__.py
+++ eckity_carbon/__init__.py
@@ -2,11 +2,11 @@ from eckity.algorithms.simple_evolution import SimpleEvolution
 from eckity.subpopulation import Subpopulation
 from examples.treegp.non_sklearn_mode.symbolic_regression.sym_reg_evaluator import SymbolicRegressionEvaluator
 
-from codecarbon import track_emissions
-from pyJoules.energy_meter import measure_energy
+from ea2p import PowerMeter
 
-@measure_energy
+
 def main():
     algo = SimpleEvolution(Subpopulation(SymbolicRegressionEvaluator()))
-    algo.evolve()
+    with PowerMeter() as meter:
+        algo.evolve()
     print(f'algo.execute(x=2,y=3,z=4): {algo.execute(x=2, y=3, z=4)}')
